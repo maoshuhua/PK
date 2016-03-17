5e2c7c1b17d6d61bbf74692646f619d6118bdc7d
maoshuhua <1083040740@qq.com>
2016-03-17 15:17:27 +0800

    解决方案初始化

---
+++ Reception/Repository/Tuhui.Reception.Repository/Reception_UserInfoRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tuhui.Common45.Framework;
+using Tuhui.Reception.Model;
+
+namespace Tuhui.Reception.Repository
+{
+    public class Reception_UserInfoRepository : BaseRepository<DbReceptionContext>, IReception_UserInfoRepository
+    {
+        //登录
+        public bool ValidationUser(Reception_UserInfo model)
+        {
+            var _result = base.ContextObj.Reception_UserInfo.Where(p => p.Name == model.Name && p.Pwd == model.Pwd).FirstOrDefault() != null;
+            
+            return _result;
+        }
+
+        //获取用户信息
+        public Reception_UserInfo GetUserByUserName(string userName)
+        {
+            var _result = base.ContextObj.Reception_UserInfo.Where(p => p.Name == userName).FirstOrDefault();
+
+            return _result;
+        }
+    }
+}
---
+++ Reception/Service/Tuhui.Reception.Service/IReception_UserInfoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tuhui.Common45.Framework;
+using Tuhui.Reception.Model;
+
+namespace Tuhui.Reception.Service
+{
+    public interface IReception_UserInfoService : IBaseService
+    {
+        //登录
+        bool ValidationUser(Reception_UserInfo model);
+
+        //获取用户信息
+        Reception_UserInfo GetUserByUserName(string userName);
+    }
+}
